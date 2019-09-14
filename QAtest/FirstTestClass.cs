01e4c346cc1340c44c7e8a99cd3e250903fad4d6
Pritom Chaki <pritom.cse.diu@gmail.com>
2019-09-14 23:53:17 +0600

    More Function Added

--- QAtest/FirstTestClass.cs
+++ QAtest/FirstTestClass.cs
@@ -2,12 +2,7 @@
 //using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace QAtest
 {
@@ -18,19 +13,19 @@ namespace QAtest
             //IWebDriver driver = new ChromeDriver();
 
             IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl("https://www.youtube.com/");
-            System.Threading.Thread.Sleep(4000);
+            //var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
             driver.Manage().Window.Maximize();
             #region Found by Xpath
-            try
-            {
-                driver.FindElement(By.XPath("//input"));
-                Console.WriteLine("Element found by XPath");
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            //try
+            //{
+            //    driver.FindElement(By.XPath("//input"));
+            //    Console.WriteLine("Element found by XPath");
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
             Console.Read();
             #endregion
             #region All link
@@ -44,6 +39,18 @@ namespace QAtest
             //element.SendKeys("Pritom Chaki");        
             //IWebElement element1 = driver.FindElement(By.Id("search-icon-legacy"));
             // element1.Click();
+            //element.Sendkeys(OpenQA.Selenium.Keys.Control + "t"); //Open New Tab
+            //var time1 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond; // Knowing Time
+            // driver.Navigate().Refresh(); //Refresh Page
+            //System.Drawing.Size windowsize = new System.Drawing.Size(480, 320); //Resize Browser Window
+            //driver.Manage().Window.Size = windowsize;
+           // Console.WriteLine(element.Displayed);  //Check the element
+            #endregion
+            #region Dropdown Value
+            //var option = driver.FindElement(By.Id("myList"));
+            //var selectElement = new SelectElement(option);
+            //Thread.Sleep(5000);
+            //selectElement.SelectByText("three");
             #endregion
             #region Page itle Mtching
             /*   
@@ -63,18 +70,41 @@ namespace QAtest
               }*/
             #endregion
             #region List Object
-           /* IWebElement ActionContainer = driver.FindElement(By.Id("global-actions"));
-            ReadOnlyCollection<IWebElement> GlobalActions = ActionContainer.FindElements(By.TagName("li"));
-            foreach (IWebElement GlobalAction in GlobalActions)
-            {
-                string ActionName = GlobalActions.Text;
-                GlobalAction.Click();
-                Thread.Sleep(5000);
-            }*/
+            /* IWebElement ActionContainer = driver.FindElement(By.Id("global-actions"));
+             ReadOnlyCollection<IWebElement> GlobalActions = ActionContainer.FindElements(By.TagName("li"));
+             foreach (IWebElement GlobalAction in GlobalActions)
+             {
+                 string ActionName = GlobalActions.Text;
+                 GlobalAction.Click();
+                 Thread.Sleep(5000);
+             }*/
             #endregion
-
-            driver.Close();
-           driver.Quit();
+            #region Java Script
+            //IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            //string title = (string)js.ExecuteScript("return document.title");
+            //Console.WriteLine(title);
+            //js.ExecuteScript("window.scrollBy(0,950);"); //Scrool the window
+            //Console.Read();
+            #endregion
+            #region Accept Alert
+            //IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            //js.ExecuteScript("alert('Hi this is alert');");
+            //Thread.Sleep(5000);
+            //IAlert alert = driver.SwitchTo().Alert();
+            //alert.Accept();
+            //Console.Read();
+            #endregion
+            #region Co Ordinate Location
+            //IWebElement element = driver.FindElement(By.Id("mp-tfa-h2"));
+            //Thread.Sleep(3000);
+            //int x = element.Location.X;
+            //int y = element.Location.Y;
+            //Console.WriteLine("X location: " + x);
+            //Console.WriteLine("Y location: " + y);
+            //Console.Read();
+            #endregion
+            //driver.Close();
+            //   driver.Quit();
         }
     }
 }
---
+++ QAtest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QAtest
+{
+    class UnitTest1
+    {
+        public IWebDriver driver;
+
+
+        public void init()
+        {
+            driver = new FirefoxDriver();
+        }
+        public void NavigatetoGoogle()
+        {
+            driver.Navigate().GoToUrl("google.com");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+        }
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
